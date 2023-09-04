clearhaus/aliquot-pay

commit cb784a7008f619b06b9bf3daa76f4b68f313f4b0

Apply suggestion from review comments

lib/aliquot-pay.rb
@@ -144,9 +144,7 @@ class AliquotPay
     return @signed_key if @signed_key
     ensure_intermediate_key
 
-    if @intermediate_key.private_key? || @intermediate_key.public_key?
-      public_key = @intermediate_key
-    else
+    if @intermediate_key.private_key? && @intermediate_key.public_key?
       fail 'Intermediate key must be public and private key'
     end
 

commit f00d709e1171ce43e2d17aacdd30234833e9f89e

Fix bug introduced in https://github.com/clearhaus/aliquot-pay/pull/12/commits/cb784a7008f619b06b9bf3daa76f4b68f313f4b0

lib/aliquot-pay.rb
@@ -144,7 +144,7 @@ class AliquotPay
     return @signed_key if @signed_key
     ensure_intermediate_key
 
-    if @intermediate_key.private_key? && @intermediate_key.public_key?
+    if !@intermediate_key.private_key? && !@intermediate_key.public_key?
       fail 'Intermediate key must be public and private key'
     end
 